ee004d1e6656554edea6be06b318b867f11f0153
Lailz <lailaabdulraheem@gmail.com>
2018-10-03 20:42:22 +0300

    starting point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+
+import "./App.css";
+import tasks from "./data";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="rectangle">
+        <p class="title">TO DO LIST</p>
+        <table class="table">
+          <tr class="headerRow">
+            <th>STAT</th>
+            <th>TASK</th>
+            <th>PRIORITY</th>
+          </tr>
+          <tr class="row">
+            <td>
+              <i className="fa fa-check-circle" style={{ color: "green" }} />
+            </td>
+            <td>Go to the supermarket</td>
+            <td style={{ color: "orange" }}>MIDDLE</td>
+          </tr>
+          <tr class="row">
+            <i className="fa fa-times-circle" style={{ color: "red" }} />
+            <td>Cook dinner</td>
+            <td style={{ color: "red" }}>HIGH</td>
+          </tr>
+          <tr class="row">
+            <i className="fa fa-times-circle" style={{ color: "red" }} />
+            <td>Call Mom</td>
+            <td style={{ color: "green" }}>LOW</td>
+          </tr>
+        </table>
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+let tasks = [
+  {
+    task: "Go to the supermarket",
+    status: true,
+    priority: "MIDDLE"
+  },
+  {
+    task: "Cook dinner",
+    status: false,
+    priority: "HIGH"
+  },
+  {
+    task: "Call Mom",
+    status: false,
+    priority: "LOW"
+  }
+];
+
+export default tasks;

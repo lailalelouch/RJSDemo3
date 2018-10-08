d4ef873f77ec7b50589b6c55e1a988addde90b4b
Octowl <alsaff1987@gmail.com>
2018-10-08 17:42:51 +0300

    Fix styling

--- src/App.js
+++ src/App.js
@@ -8,31 +8,39 @@ import tasks from "./data";
 class App extends Component {
   render() {
     return (
-      <div class="rectangle">
-        <p class="title">TO DO LIST</p>
-        <table class="table">
-          <tr class="headerRow">
-            <th>STATUS</th>
-            <th>TASK</th>
-            <th>PRIORITY</th>
-          </tr>
-          <tr class="row">
-            <td>
-              <i className="fa fa-check-circle" />
-            </td>
-            <td>Go to the supermarket</td>
-            <td className="middle">MIDDLE</td>
-          </tr>
-          <tr class="row">
-            <i className="fa fa-times-circle" />
-            <td>Cook dinner</td>
-            <td className="high">HIGH</td>
-          </tr>
-          <tr class="row">
-            <i className="fa fa-times-circle" />
-            <td>Call Mom</td>
-            <td className="low">LOW</td>
-          </tr>
+      <div className="rectangle">
+        <p className="title">TO DO LIST</p>
+        <table>
+          <thead>
+            <tr>
+              <th>STATUS</th>
+              <th>TASK</th>
+              <th>PRIORITY</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <i className="fa fa-check-circle" />
+              </td>
+              <td>Go to the supermarket</td>
+              <td className="middle">MIDDLE</td>
+            </tr>
+            <tr>
+              <td>
+                <i className="fa fa-times-circle" />
+              </td>
+              <td>Cook dinner</td>
+              <td className="high">HIGH</td>
+            </tr>
+            <tr>
+              <td>
+                <i className="fa fa-times-circle" />
+              </td>
+              <td>Call Mom</td>
+              <td className="low">LOW</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );

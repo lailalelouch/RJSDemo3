664274798f0d522a870048a74b875c16a338b372
Octowl <alsaff1987@gmail.com>
2018-10-14 14:59:17 +0300

    Move statusIcon out of App.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,10 @@ import tasks from "./data";
 import TodoList from "./TodoList";
 
 function App() {
-  const statusIcon = done => {
-    if (done) {
-      return "fa fa-check-circle";
-    } else {
-      return "fa fa-times-circle";
-    }
-  };
-
   return (
     <div className="rectangle">
       <p className="title">TO DO LIST</p>
-      <Table tasks={tasks} />
+      <TodoList tasks={tasks} />
     </div>
   );
 }

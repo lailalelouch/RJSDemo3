1d01c303431988719a6ceaf5cd10033b6de7545a
Octowl <alsaff1987@gmail.com>
2018-10-14 14:55:52 +0300

    Move statusIcon function to TodoItem

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,18 +5,19 @@ import "./App.css";
 class TableRow extends Component {
   render() {
     const task = this.props.task;
-    let statusIcon;
 
-    if (task.done) {
-      statusIcon = "fa fa-check-circle";
-    } else {
-      statusIcon = "fa fa-times-circle";
-    }
+    const statusIcon = done => {
+      if (done) {
+        return "fa fa-check-circle";
+      } else {
+        return "fa fa-times-circle";
+      }
+    };
 
     return (
       <tr className="row">
         <td>
-          <i className={statusIcon} />
+          <i className={statusIcon(task.done)} />
         </td>
         <td>{task.task}</td>
         <td className={task.priority}>{task.priority.toUpperCase()}</td>

6e79d9b769440a9dc0a3ffe6d26fa338f0675361
Octowl <alsaff1987@gmail.com>
2018-10-16 18:37:34 +0300

    update data.js

--- src/data.js
+++ src/data.js
@@ -1,20 +1,24 @@
 let tasks = [
   {
+    id: 1,
     task: "Go to the supermarket",
     done: true,
     priority: "middle"
   },
   {
+    id: 2,
     task: "Cook dinner",
     done: false,
     priority: "high"
   },
   {
+    id: 3,
     task: "Call Mom",
     done: false,
     priority: "low"
   },
   {
+    id: 4,
     task: "Study for test",
     done: true,
     priority: "high"

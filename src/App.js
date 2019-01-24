0623690d5c3009779e030a0f35d38792c0794e8f
Octowl <alsaff1987@gmail.com>
2019-01-24 20:45:24 +0300

    fix import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import "../node_modules/font-awesome/css/font-awesome.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 import "./App.css";
 import tasks from "./data";

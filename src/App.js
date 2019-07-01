7d01579050224ea0fdbd6b0c4f4cfab1a7d93642
Lailz <lailaabdulraheem@gmail.com>
2019-07-01 17:35:08 +0300

    change class-based component to function

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "font-awesome/css/font-awesome.min.css";
 
 import "./App.css";
-import tasks from "./data";
 
-class App extends Component {
-  render() {
-    const statusIcon = done => {
-      if (done) {
-        return "fa fa-check-circle";
-      } else {
-        return "fa fa-times-circle";
-      }
-    };
+function App() {
+  const statusIcon = done => {
+    if (done) {
+      return "fa fa-check-circle";
+    } else {
+      return "fa fa-times-circle";
+    }
+  };
 
-    return (
-      <div className="rectangle">
-        <p className="title">TO DO LIST</p>
-        <table>
-          <thead>
-            <tr>
-              <th>STATUS</th>
-              <th>TASK</th>
-              <th>PRIORITY</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <i className={statusIcon(true)} />
-              </td>
-              <td>Go to the supermarket</td>
-              <td className="middle">MIDDLE</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+  return (
+    <div className="rectangle">
+      <p className="title">TO DO LIST</p>
+      <table>
+        <thead>
+          <tr>
+            <th>STATUS</th>
+            <th>TASK</th>
+            <th>PRIORITY</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <i className={statusIcon(true)} />
+            </td>
+            <td>Go to the supermarket</td>
+            <td className="middle">MIDDLE</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;

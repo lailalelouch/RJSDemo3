f0f2456fc6df3512c12fe46f9d8930ff9d7b4f19
Octowl <alsaff1987@gmail.com>
2019-07-04 17:02:25 +0300

    Make components functions

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,32 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
-import "./App.css";
+function ToDoItem(props) {
+  const item = props.item;
 
-class TableRow extends Component {
-  render() {
-    const task = this.props.task;
+  const statusIcon = () => {
+    if (item.done) {
+      return "fa fa-check-circle";
+    } else {
+      return "fa fa-times-circle";
+    }
+  };
 
-    const statusIcon = () => {
-      if (task.done) {
-        return "fa fa-check-circle";
-      } else {
-        return "fa fa-times-circle";
-      }
-    };
-
-    return (
-      <tr className="row">
-        <td>
-          <i className={statusIcon()} />
-        </td>
-        <td>{task.task}</td>
-        <td className={task.priority}>{task.priority.toUpperCase()}</td>
-        <td>
-          <i className="fa fa-times" />
-        </td>
-      </tr>
-    );
-  }
+  return (
+    <tr className="row">
+      <td>
+        <i className={statusIcon()} />
+      </td>
+      <td>{item.task}</td>
+      <td className={item.priority}>{item.priority.toUpperCase()}</td>
+      <td>
+        <i className="fa fa-times" />
+      </td>
+    </tr>
+  );
 }
 
-export default TableRow;
+export default ToDoItem;
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,28 +1,25 @@
-import React, { Component } from "react";
-
-import "./App.css";
+import React from "react";
 
 import ToDoItem from "./ToDoItem";
 
-class ToDoList extends Component {
-  render() {
-    let taskRows = this.props.tasks.map(task => (
-      <ToDoItem task={task} key={task.task} />
-    ));
+function ToDoList(props) {
+  const taskRows = props.tasks.map(item => (
+    <ToDoItem item={item} key={item.task} />
+  ));
 
-    return (
-      <table className="table">
-        <thead>
-          <tr className="headerRow">
-            <th>STATUS</th>
-            <th>TASK</th>
-            <th>PRIORITY</th>
-            <th>DELETE</th>
-          </tr>
-        </thead>
-        <tbody>{taskRows}</tbody>
-      </table>
-    );
-  }
+  return (
+    <table className="table">
+      <thead>
+        <tr className="headerRow">
+          <th>STATUS</th>
+          <th>TASK</th>
+          <th>PRIORITY</th>
+          <th>DELETE</th>
+        </tr>
+      </thead>
+      <tbody>{taskRows}</tbody>
+    </table>
+  );
 }
+
 export default ToDoList;
